cfccc364d04b559de998873716085ec053e3b051
saudag <saudaghatge09@gmail.com>
2024-03-26 02:20:39 -0400

    classes

--- scripts/FSM.py
+++ scripts/FSM.py
@@ -1,8 +1,3 @@
-
-'''
-CLEAN THE CODE STUPID GIRL!!!!!!!
-'''
-
 import os
 import sys
 import time
@@ -41,50 +36,54 @@ from act_pump.srv import service1
 """
 UPDATE Readme - asap
 """
-
-class FSM:
+class Utils:
 
     def __init__(self):
-
-        # dictionary of nearby cornstalks
+        print("Utils class")
         self.near_cs = {}
 
+    # GetStalk is the .srv file
+    # get_stalk: name of the service being called
+    # stalk: service client object
+    # output_1: this has three outputs - string(success), int(num_frames), stalk_detect/grasp_point[] (grasp_points) {grasp_points is an array of type stalk_detect (which is basically x,y,z coordinate)}
+    # TODO: (should be accessible from all the methods of the class) near_cs: unordered list (dict) of nearby cornstalks with key = hashvalue of grasppoints
     def get_grasp (self, num_frames, timeout):
-        rospy.loginfo('Finding nearest Cornstalk')
 
+        rospy.loginfo('Finding nearest Cornstalk')
         rospy.wait_for_service('get_stalk')
-        # GetStalk is the .srv file
-        # get_stalk: name of the service being called
-        # stalk: service client object
-        # output_1: this has three outputs - string(success), int(num_frames), stalk_detect/grasp_point[] (grasp_points) {grasp_points is an array of type stalk_detect (which is basically x,y,z coordinate)}
-        # TODO: (should be accessible from all the methods of the class) near_cs: unordered list (dict) of nearby cornstalks with key = hashvalue of grasppoints
         stalk = rospy.ServiceProxy('get_stalk', GetStalk)
+
         try:
+            print("in try")
             output_1 = stalk(num_frames=num_frames, timeout=timeout)
             flag = output_1.success
             grasp_points = output_1.grasp_points
+            print(f"is this working? {flag}")
 
-            if (flag == "SUCCESS"):
+            # for i,point in enumerate(grasp_points):
+            #     print(f"Grasp Point {i}: x={point.position.x}, y={point.position.y}, z={point.position.z}")
 
-                for i,point in enumerate(grasp_points):
-                    grasp_coordinates = (point.position.x, point.position.y, point.position.z)
-                    
-                    if grasp_coordinates in self.near_cs:
-                        print("Print the grasp_coordinates: x: %f, y: %f, z: %f", point.position.x, point.position.y, point.position.z)
-                        print("Print the near_cs list")
-                        print(self.near_cs)
-                        print("Corn already visited")
-                        continue #if point is in the near_ss dictionary, then continue to the next grasp_point 
+            # if (flag == "SUCCESS"):
 
-                    else:
-                        self.near_cs[grasp_coordinates] = point
-                        rospy.loginfo('Point %d - x: %f, y: %f, z: %f', i, point.position.x, point.position.y, point.position.z)
+            #     for i,point in enumerate(grasp_points):
+            #         grasp_coordinates = (point.position.x, point.position.y, point.position.z)
+            #         print("Print the grasp_coordinates: x: %f, y: %f, z: %f", point.position.x, point.position.y, point.position.z)
+            #         if grasp_coordinates in self.near_cs:
+            #             print("Print the grasp_coordinates: x: %f, y: %f, z: %f", point.position.x, point.position.y, point.position.z)
+            #             print("Print the near_cs list")
+            #             print(self.near_cs)
+            #             print("Corn already visited")
+            #             continue #if point is in the near_cs dictionary, then continue to the next grasp_point 
+
+            #         else:
+            #             self.near_cs[grasp_coordinates] = point
+            #             rospy.loginfo('Point %d - x: %f, y: %f, z: %f', i, point.position.x, point.position.y, point.position.z)
 
         except rospy.ServiceException as exc:
             rospy.loginfo('Service did not process request: ' + str(exc))
 
         return flag
-
+    
     # def suitable_width (self, num_frames, timeout):  # m_2 --> suitable_width
     #     rospy.loginfo('Doing Width Detection')
     #     rospy.wait_for_service('get_width')
@@ -101,7 +100,7 @@ class FSM:
     #     return flag
 
     # def m_3 (self, pumps):
-        
+
     #     rospy.loginfo('Go to Clean Nozzle')
     #     rospy.wait_for_service('control_pumps')
     #     xarm_status = rospy.ServiceProxy('control_pumps', service1)
@@ -113,98 +112,79 @@ class FSM:
     #         rospy.loginfo('Service did not process request: ' + str(exc))
     #     return status
 
-    # def m_4 ():
-    #     rospy.loginfo('Go to Calibrate Nozzle')
+# State 1 - Finding Cornstalk
+#TODO: error handling. Proceed to grasp_stalk only after success flag
+class state1(smach.State):
 
-    # def m_5 ():
-    #     rospy.loginfo('Logging Data')
-
-    # def m_6 ():
-    #     rospy.loginfo('Desired Arc Movement')
-
-    # def m_7 ():
-    #     rospy.loginfo('Insert Sensor-Actuate Linear Actuator')
-
-    # def m_8 ():
-    #     rospy.loginfo('Remove Sensor-Retract Linear Actuator')
-
-    # def m_9 ():
-    #     rospy.loginfo('Move arm to the nearest cornstalk')
-
-    # def stow_position ():
-    #     rospy.loginfo('Stow Position')
-
-    # def m_11 ():
-    #     rospy.loginfo('Replace Sensor')
-
-
-    # State 1 - Finding Cornstalk
-    #TODO: error handling. Proceed to grasp_stalk only after success flag
-    class state1(smach.State):
-
-        def __init__(self):
-            smach.State.__init__(self,
-                                outcomes = ['cleaning_calibrating'],
-                                input_keys = ['state_1_input'])
-            
-        def execute(self, userdata):
-            rospy.loginfo('Running State 1: Finding Cornstalk')
-
-            # xarm_flag = stow_position()
-
-            grasp_flag = self.get_grasp(self, userdata.state_1_input[0], userdata.state_1_input[1])
-            print("Output from find_stalk method: %s", grasp_flag)
-            
-            # flag_2 = m_2(self, userdata.state_1_input[0], userdata.state_1_input[1])
-
-            # rospy.INFO('Output from find_width method: %s', flag_2)
-            # print("Output from find_stalk method: %s", flag_2)
-            return 'cleaning_calibrating'
-
-    # State 2 - Cleaning and Calibrating
-    class state2(smach.State):
-
-        def __init__(self):
-            smach.State.__init__(self,
-                                outcomes = ['insertion','replace'],
-                                input_keys = ['state_2_ip'])
-            
-        def execute(self, userdata):
-            rospy.loginfo('Running State 2')
-
-            return 'insertion'
+    def __init__(self, utils):
+        smach.State.__init__(self,
+                            outcomes = ['cleaning_calibrating'],
+                            input_keys = ['state_1_input'])
+        self.utils = utils
         
-    # State 3: Insertion
-    class state3(smach.State):
+    def execute(self, userdata):
+        rospy.loginfo('Running State 1: Finding Cornstalk')
 
-        def __init__(self):
-            smach.State.__init__(self,
-                                outcomes = ['replace'],
-                                input_keys = ['state_3_ip'])
+        # xarm_flag = stow_position()
+
+        grasp_flag = self.utils.get_grasp(userdata.state_1_input[0], userdata.state_1_input[1])
+        print("Output from find_stalk method: %s", grasp_flag)
         
-        def execute(self, userdata):
-            rospy.loginfo('Running State 3')
+        # flag_2 = m_2(self, userdata.state_1_input[0], userdata.state_1_input[1])
 
-            # flag = m_3(self, userdata.state_3_ip)
-            # print("Output from EM method: %s", flag)
-            return 'replace'
-        
-    # State 4: Replace
-    class state4(smach.State):
+        # rospy.INFO('Output from find_width method: %s', flag_2)
+        # print("Output from find_stalk method: %s", flag_2)
+        return 'cleaning_calibrating'
 
-        def __init__(self):
-            smach.State.__init__(self,
-                                outcomes = ['replace_stop'])
-        
-        def execute(self, userdata):
-            rospy.loginfo('Running State 3')
-            # flag = m_3(self, userdata.state4_ip)
-            # print("Output from EM method: %s", flag)
-            return 'replace_stop'
+# State 2 - Cleaning and Calibrating
+class state2(smach.State):
 
-            
-    def main():
+    def __init__(self, utils):
+        smach.State.__init__(self,
+                            outcomes = ['insertion','replace'],
+                            input_keys = ['state_2_ip'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Running State 2')
+
+        return 'insertion'
+
+# State 3: Insertion
+class state3(smach.State):
+
+    def __init__(self, utils):
+        smach.State.__init__(self,
+                            outcomes = ['replace'],
+                            input_keys = ['state_3_ip'])
+
+    def execute(self, userdata):
+        rospy.loginfo('Running State 3')
+
+        # flag = m_3(self, userdata.state_3_ip)
+        # print("Output from EM method: %s", flag)
+        return 'replace'
+
+# State 4: Replace
+class state4(smach.State):
+
+    def __init__(self, utils):
+        smach.State.__init__(self,
+                            outcomes = ['replace_stop'])
+    
+    def execute(self, userdata):
+        rospy.loginfo('Running State 3')
+        # flag = m_3(self, userdata.state4_ip)
+        # print("Output from EM method: %s", flag)
+        return 'replace_stop'
+
+class FSM:
+
+    def __init__(self):
         rospy.init_node('nimo_state_machine')
+        self.utils = Utils()
+        self.main()
+
+    def main(self):
 
         start_state = smach.StateMachine(outcomes = ['stop'])    # Outcome of Main State Machine
         start_state.userdata.flag_a = 1
@@ -215,15 +195,15 @@ class FSM:
 
         with start_state:
 
-            smach.StateMachine.add('Finding_Cornstalk',FSM.state1(),
+            smach.StateMachine.add('Finding_Cornstalk',state1(self.utils),
                                 transitions = {'cleaning_calibrating':'Cleaning_Calibrating'},
                                 remapping = {'state_1_input':'find_stalk'})  # Go to State B
             
-            smach.StateMachine.add('Cleaning_Calibrating',FSM.state2(),
+            smach.StateMachine.add('Cleaning_Calibrating',state2(self.utils),
                                 transitions = {'insertion':'Insertion','replace':'stop'},
                                 remapping = {'c_c_ip':'flag_b'})  # Go to State B
             
-            smach.StateMachine.add('Insertion',FSM.state3(),
+            smach.StateMachine.add('Insertion',state3(self.utils),
                                 transitions = {'replace':'stop'},
                                 remapping = {'state_3_ip':'gopump'})  # Go to State B
             
@@ -239,4 +219,3 @@ class FSM:
 
 if __name__ == '__main__':
     fsm = FSM()
-    fsm.main()
